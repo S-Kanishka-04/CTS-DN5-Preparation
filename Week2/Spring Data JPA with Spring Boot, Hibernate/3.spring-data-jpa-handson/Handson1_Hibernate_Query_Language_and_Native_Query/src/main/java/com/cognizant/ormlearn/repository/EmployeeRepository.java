e3cc11546e9529ba2563f7880238fa44a655d3cb
S Kanishka <kanishkashankar71@gmail.com>
2026-07-01 21:07:37 +0530

    Completed Spring Data JPA Hands-on 2 - HQL query with JOIN FETCH

---
+++ Week2/Spring Data JPA with Spring Boot, Hibernate/3.spring-data-jpa-handson/Handson1_Hibernate_Query_Language_and_Native_Query/src/main/java/com/cognizant/ormlearn/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.cognizant.ormlearn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.cognizant.ormlearn.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
+
+	//Simple HQL
+	//@Query("SELECT e FROM Employee e WHERE e.permanent = true")
+	
+	//LEFT JOIN (without FETCH)
+	//@Query("SELECT e FROM Employee e LEFT JOIN e.department d LEFT JOIN e.skillList WHERE e.permanent = true")
+	
+	//LEFT JOIN FETCH (final optimized version)
+	@Query("SELECT e FROM Employee e LEFT JOIN FETCH e.department LEFT JOIN FETCH e.skillList WHERE e.permanent = true")
+	List<Employee> getAllPermanentEmployees();
+}
