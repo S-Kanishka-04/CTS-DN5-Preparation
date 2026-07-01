b0156e97c2733c732dc7324cdf3a9619e5da2c8a
S Kanishka <kanishkashankar71@gmail.com>
2026-07-01 10:00:02 +0530

    Added Week2 Spring Data JPA Hands-on 1 -Query Methods

---
+++ Week2/Spring Data JPA with Spring Boot, Hibernate/2.spring-data-jpa-handson/Handson1_Query_Methods/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,135 @@
+package com.cognizant.ormlearn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+
+        countryService = context.getBean(CountryService.class);
+
+        LOGGER.info("Inside main");
+        
+        //testAddCountry();
+        //testGetAllCountries();
+        //testFindCountryByCode();
+
+        testSearchCountry();
+
+        //testSearchCountrySorted();
+
+        //testSearchCountryStartingWith();
+
+    }
+
+    // ---------------------- Previous Hands-on ----------------------
+
+    private static void testAddCountry() {
+
+        LOGGER.info("Start");
+
+        Country country = new Country();
+        country.setCode("NP");
+        country.setName("Nepal");
+
+        countryService.addCountry(country);
+
+        try {
+            Country newCountry = countryService.findCountryByCode("NP");
+            LOGGER.debug("Country={}", newCountry);
+        } catch (CountryNotFoundException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        LOGGER.info("End");
+    }
+
+    private static void testGetAllCountries() {
+
+        LOGGER.info("Start");
+
+        List<Country> countries = countryService.getAllCountries();
+
+        LOGGER.debug("Countries={}", countries);
+
+        LOGGER.info("End");
+    }
+
+    private static void testFindCountryByCode() {
+
+        LOGGER.info("Start");
+
+        try {
+            Country country = countryService.findCountryByCode("IN");
+            LOGGER.debug("Country={}", country);
+        } catch (CountryNotFoundException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        LOGGER.info("End");
+    }
+    
+    // Query Method 1
+    // Search countries containing "ou"
+
+    private static void testSearchCountry() {
+
+        LOGGER.info("Start");
+
+        List<Country> countries = countryService.searchCountries("ou");
+
+        for (Country country : countries) {
+            LOGGER.debug("Country={}", country);
+        }
+        LOGGER.info("End");
+    }
+
+    // Query Method 2
+    // Search countries containing "ou" in ascending order
+
+    private static void testSearchCountrySorted() {
+
+        LOGGER.info("Start");
+
+        List<Country> countries = countryService.searchCountriesSorted("ou");
+
+        for (Country country : countries) {
+            LOGGER.debug("Country={}", country);
+        }
+
+        LOGGER.info("End");
+    }
+
+    // Query Method 3
+    // Search countries starting with "Z"
+
+    private static void testSearchCountryStartingWith() {
+
+        LOGGER.info("Start");
+
+        List<Country> countries = countryService.searchCountriesStartingWith("Z");
+
+        for (Country country : countries) {
+            LOGGER.debug("Country={}", country);
+        }
+
+        LOGGER.info("End");
+    }
+}
